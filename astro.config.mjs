74c739c1f9a13f35b506d71f2631420da02b333b
Natsa <claudio.natsa@gmail.com>
2025-01-29 21:08:37 -0300

    se agrega cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://novacode.com.ar',
   base: '/',
   trailingSlash: 'never',
+
   build: {
     format: 'file'
-  }
+  },
+
+  adapter: cloudflare()
 });
